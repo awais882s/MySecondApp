2fcc27baca7719ea1462e8f23e5258823361856f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-30 18:04:22 +0500

    Update Home.js

--- src/screens/Frontend/Home.js
+++ src/screens/Frontend/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
     ]
     return (
 
-        <View style={[styles.flexContainer, styles.flexCenter, { backgroundColor: "pink" }]}>
+        <View style={[styles.flexContainer, { paddingHorizontal: 12 }]}>
             <FlatList
                 style={styles.flexContainer}
                 data={items}
