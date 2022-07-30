10b6f26567bf9a0147592ed52fb7e450527e3189
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-30 17:21:03 +0500

    Update index.js

--- src/screens/Frontend/index.js
+++ src/screens/Frontend/index.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Gallery from './Gallery'
+import Home from './Home'
 
 export default function Frontend() {
     return (
         <>
-            <Gallery />
+            <Home />
         </>
     )
 }
