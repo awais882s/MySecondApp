15416e9bed4993fefdb95573875a9ff727a6f79d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-30 23:49:30 +0500

    Update Contact.js

--- src/screens/Frontend/Contact.js
+++ src/screens/Frontend/Contact.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
         // justifyContent: "center",
         // alignItems: "center",
         backgroundColor: "white",
-        flex: 1
+        flex: 1 
     },
     h1: {
         fontSize: 48,
