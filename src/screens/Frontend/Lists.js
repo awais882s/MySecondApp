439bb1ed2375059431bd756a3c04f90670005045
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-31 14:54:16 +0500

    sectionList

--- src/screens/Frontend/Lists.js
+++ src/screens/Frontend/Lists.js
@@ -1,21 +1,52 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, SectionList } from 'react-native'
 export default function Lists() {
+
+    // for sections list
     const items = [
-        { id: '0', text: 'Working' },
-        { id: '1', text: 'Text' },
-        { id: '2', text: 'Image' },
-        { id: '3', text: 'ScrollView' },
-        { id: '4', text: 'ListView' },
+        {
+            id: '0',
+            title: 'Basic Components',
+            data: [
+                { id: '0', text: 'View' },
+                { id: '1', text: 'Text' },
+                { id: '2', text: 'Image' },
+            ],
+        },
+        {
+            id: '1',
+            title: 'List Components',
+            data: [
+                { id: '3', text: 'ScrollView' },
+                { id: '4', text: 'ListView' },
+            ],
+        },
     ]
+    // for flat lsit 
+    // const items = [
+    //     { id: '0', text: 'Working' },
+    //     { id: '1', text: 'Text' },
+    //     { id: '2', text: 'Image' },
+    //     { id: '3', text: 'ScrollView' },
+    //     { id: '4', text: 'ListView' },
+    // ]
     return (
-        <View style={[styles.flexContainer, { paddingHorizontal: 12 }]}>
-            <FlatList
+        <View style={[styles.flexContainer]}>
+            <SectionList
+                style={styles.container}
+                sections={items}
+                renderItem={({ item }) => <Text style={styles.row}>{item.text}</Text>}
+                renderSectionHeader={({ section }) => (
+                    <Text style={styles.header}>{section.title}</Text>
+                )}
+                keyExtractor={(item) => item.id}
+            />
+            {/* <FlatList
                 style={styles.flexContainer}
                 data={items}
                 renderItem={({ item }) => <Text style={styles.row}>{item.text}</Text>}
                 keyExtractor={(item) => item.id}
-            />
+            /> */}
             {/* <View style={[styles.box, styles.flexCenter]}>
                 <Text style={styles.h1}>Lists</Text>
             </View> */}
