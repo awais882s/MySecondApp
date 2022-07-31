bad83d28af4d2b5e0b6098fb43262c806d147742
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-31 16:42:01 +0500

    app navigators

---
+++ src/navigations/AppNavigators.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AppNavigators() {
+    return (
+        <View>
+            <Text>AppNavigators</Text>
+        </View>
+    )
+}
