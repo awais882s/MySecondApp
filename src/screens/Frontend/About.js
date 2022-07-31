4a2ac2ea20b3f9b26a096ecd8f3b7e68607d3752
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-31 18:28:47 +0500

    go to contact

--- src/screens/Frontend/About.js
+++ src/screens/Frontend/About.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-export default function About() {
+import { View, Text, StyleSheet, Button } from 'react-native'
+export default function About({ navigation }) {
     return (
         <View style={styles.flexContainer}>
             <Text style={styles.h1}>About Us</Text>
+            <Button title='Go To Conatct' onPress={() => { navigation.navigate("Contact") }} />
         </View>
     )
 }
 const styles = StyleSheet.create({
     flexContainer: {
-        // justifyContent: "center",
-        // alignItems: "center",
+        justifyContent: "center",
+        alignItems: "center",
         backgroundColor: "white",
         flex: 1
     },
