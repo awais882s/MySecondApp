dbb89c52eb4972d1839388b0c7450fb8995afa0e
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-31 15:25:17 +0500

    status bar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Frontend from './src/screens/Frontend'
+import Frontend from './src/screens/Frontend';
+import { StatusBar } from 'react-native';
 // import Home from './src/screens/Frontend/Home'
 export default function App() {
   return (
     <>
+      <StatusBar backgroundColor="#7d5fff" barStyle="dark-content" />
       <Frontend />
     </>
   )
