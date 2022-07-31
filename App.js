d250cd48e10ced8b6dbb6f2c896039c17e762296
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-31 16:43:49 +0500

    navigators

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Frontend from './src/screens/Frontend';
+// import Frontend from './src/screens/Frontend';
 import { StatusBar } from 'react-native';
+import AppNavigators from './src/navigations/AppNavigators';
 // import Home from './src/screens/Frontend/Home'
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor="#7d5fff" barStyle="dark-content" />
-      <Frontend />
+      <AppNavigators />
+      {/* <Frontend /> */}
     </>
   )
 }
