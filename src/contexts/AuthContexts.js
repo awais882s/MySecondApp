0097736925f2fec525d58c1ece64c8ef050049bb
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-08-03 15:08:32 +0500

    use Context

---
+++ src/contexts/AuthContexts.js
@@ -0,0 +1,21 @@
+import React, { useReducer, createContext } from 'react'
+const AuthContext = createContext()
+const intialState = { isAuthenticated: false }
+const reducer = (state, { type }) => {
+
+    switch (type) {
+        case "LOGIN":
+            return Object.assign({}, { isAuthenticated: true })
+        case "LOGOUT":
+            return Object.assign({}, { isAuthenticated: false })
+    }
+
+}
+
+export default function AuthContextsProvider(props) {
+    return (
+        <AuthContext.Provider>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
