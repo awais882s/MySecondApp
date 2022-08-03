ae55d130d6c61c8d60a6fe84b7b278083e9fc123
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-08-03 20:24:36 +0500

    Update Login.js

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+const intialState = { email: "", password: "" }
 
 export default function Login() {
+    const [state, setState] = useState(intialState)
+    const handleChange = () => {
+        setState(s => ({ ...s, [e.target.name]: e.target.value }))
+    }
     return (
         <View style={styles.flexCenter}>
             <Text style={styles.h1}>Login</Text>
